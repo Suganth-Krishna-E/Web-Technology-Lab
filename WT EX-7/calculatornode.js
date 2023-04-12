2eaec3928465cb34f381850d26649da35f95439b
Suganth-Krishna-E <127000447+Suganth-Krishna-E@users.noreply.github.com>
2023-04-12 08:39:41 +0530

    Add files via upload

---
+++ WT EX-7/calculatornode.js
@@ -0,0 +1,25 @@
+exports.addition=function(a,b)
+{
+    return a+b;
+}
+
+exports.subtraction=function(a,b)
+{
+    return a-b;
+}
+
+exports.multiplication=function(a,b)
+{
+    return a*b;
+}
+
+exports.division=function(a,b)
+{
+    return a/b;
+}
+
+exports.modulodiv=function(a,b)
+{
+    return a%b;
+}
+
